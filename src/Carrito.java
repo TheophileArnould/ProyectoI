4bd68410300cc9439969afe5cec1cf1b36779232
samumedigo <142108984+samumedigo@users.noreply.github.com>
2023-09-25 10:23:23 -0500

    Update Carrito.java

--- src/Carrito.java
+++ src/Carrito.java
@@ -1,28 +1,43 @@
 import items.Ropa;
-
 import java.util.ArrayList;
 
 public class Carrito {
     private ArrayList<Ropa> carritoContent;
 
+    // Constructor para inicializar el carrito
+    public Carrito() {
+        carritoContent = new ArrayList<>();
+    }
+
+    // Método para agregar un elemento al carrito
     public void addToCarrito(Ropa r){
         carritoContent.add(r);
     }
 
-    public void removefromCarrito(Ropa r){
+    // Método para eliminar un elemento del carrito
+    public void removeFromCarrito(Ropa r){
         carritoContent.remove(r);
     }
 
+    // Método para mostrar el contenido del carrito
     public void seeCarrito(){
-
+        if (carritoContent.isEmpty()) {
+            System.out.println("El carrito está vacío.");
+        } else {
+            System.out.println("Contenido del carrito:");
+            for (Ropa r : carritoContent) {
+                System.out.println("- " + r.getNombre() + " - Precio: " + r.getPrecio());
+            }
+        }
     }
 
+    // Método para calcular el precio total del carrito
     public double carritoPrecio(){
         double precio = 0;
-        for (Ropa r: carritoContent) {
+        for (Ropa r : carritoContent) {
             precio += r.getPrecio();
         }
-
-        return  precio;
+        return precio;
     }
 }
+
