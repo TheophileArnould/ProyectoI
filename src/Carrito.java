ef48516b5274fb5d3050d603cf73be888edb821d
samumedigo <142108984+samumedigo@users.noreply.github.com>
2023-09-25 11:16:22 -0500

    Update Carrito.java

--- src/Carrito.java
+++ src/Carrito.java
@@ -10,17 +10,17 @@ public class Carrito {
     }
 
     // Método para agregar un elemento al carrito
-    public void addToCarrito(Ropa r){
+    public void addToCarrito(Ropa r) {
         carritoContent.add(r);
     }
 
     // Método para eliminar un elemento del carrito
-    public void removeFromCarrito(Ropa r){
+    public void removeFromCarrito(Ropa r) {
         carritoContent.remove(r);
     }
 
     // Método para mostrar el contenido del carrito
-    public void seeCarrito(){
+    public void seeCarrito() {
         if (carritoContent.isEmpty()) {
             System.out.println("El carrito está vacío.");
         } else {
@@ -32,12 +32,33 @@ public class Carrito {
     }
 
     // Método para calcular el precio total del carrito
-    public double carritoPrecio(){
+    public double carritoPrecio() {
         double precio = 0;
         for (Ropa r : carritoContent) {
             precio += r.getPrecio();
         }
         return precio;
+    }
+
+    // Método para agregar un elemento del mercado al carrito
+    public void agregarAlCarritoDesdeMercado(App app, int indice) {
+        if (indice >= 0 && indice < app.ropaDisponible.size()) {
+            Ropa ropaSeleccionada = app.ropaDisponible.get(indice);
+            addToCarrito(ropaSeleccionada);
+            System.out.println("Has agregado " + ropaSeleccionada.getNombre() + " al carrito.");
+        } else {
+            System.out.println("Índice no válido.");
+        }
+    }
+
+    // Método para eliminar un elemento del carrito basado en su índice en el carrito
+    public void eliminarDelCarrito(int indice) {
+        if (indice >= 0 && indice < carritoContent.size()) {
+            Ropa ropaEliminada = carritoContent.remove(indice);
+            System.out.println("Has eliminado " + ropaEliminada.getNombre() + " del carrito.");
+        } else {
+            System.out.println("Índice no válido.");
+        }
     }
 }
 
